a5be8a51f815f60c0b136d10470adeba06d2f4c9
Victor <vk_220@hotmail.com>
2020-01-12 20:20:46 +0200

    upload new folder

---
+++ Exercise_2-node restful api/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const server = express();
+
+server.get("/weather", (request, response) => {
+    response.send("Cool Weather!");
+});
+
+server.get("/", (request, response) => {
+    response.send("Welcome to our Meteorology Website .");
+});
+
+server.get("/weather-by-city/:city", (request, response) => {
+    const city = request.params.city;
+    response.send("It is cool weather in " + city + " tonight .");
+});
+
+server.get("/weather-by-country/:country", (request, response) => {
+    const country = request.params.country;
+    response.send("It is cool weather in " + country + " this morning .");
+});
+
+server.listen(3000, () => console.log("Listening on http://localhost:3000"));
